19e98d2aa006ebfb25f38b5b0b5d9137c04d4200
Ali Çiçekçioğlu <ugurali358@gmail.com>
2023-03-04 18:05:23 +0300

    Menü düzenlemesi

--- Menu/Menu.py
+++ Menu/Menu.py
@@ -1,18 +1,101 @@
 import pandas as pd
 
 
-def get_menu():
-    pizza = pd.read_csv("pizzas.cvs")
-    print(pizza[["Pizzas", "Price"]].to_string(index=False))
-
-    ingredients = pd.read_csv("ingredients.cvs")
-    print(ingredients[["Ingredients", "Price"]].to_string(index=False))
-
-    beverages = pd.read_csv("beverages.cvs")
-    print(beverages[["Beverages", "Price"]].to_string(index=False, justify="left"))
-
-    sauces = pd.read_csv("sauces.cvs")
-    print(sauces[["Sauces", "Price"]].to_string(index=False, justify="left"))
+# Pizzalar
+def klasik_pizza_cost():
+    pizza = pd.read_csv("pizzas.csv")
+    return pizza.iloc[0, 1]
 
 
-get_menu()
+def margarita_pizza_cost():
+    pizza = pd.read_csv("pizzas.csv")
+    return pizza.iloc[1, 1]
+
+
+def turk_pizza_cost():
+    pizza = pd.read_csv("pizzas.csv")
+    return pizza.iloc[2, 1]
+
+
+def sade_pizza_cost():
+    pizza = pd.read_csv("pizzas.csv")
+    return pizza.iloc[3, 1]
+
+
+# Soslar
+def ketcap_cost():
+    sauce = pd.read_csv("sauces.csv")
+    return sauce.iloc[0, 1]
+
+
+def mayonez_cost():
+    sauce = pd.read_csv("sauces.csv")
+    return sauce.iloc[1, 1]
+
+
+def hardal_cost():
+    sauce = pd.read_csv("sauces.csv")
+    return sauce.iloc[2, 1]
+
+
+def ranch_cost():
+    sauce = pd.read_csv("sauces.csv")
+    return sauce.iloc[3, 1]
+
+
+def bbq_cost():
+    sauce = pd.read_csv("sauces.csv")
+    return sauce.iloc[4, 1]
+
+
+def aci_sos_cost():
+    sauce = pd.read_csv("sauces.csv")
+    return sauce.iloc[5, 1]
+
+#ingredients
+
+def zeytin_cost():
+    ingredients = pd.read_csv("ingredients.csv")
+    return ingredients.iloc[0, 1]
+
+def mantar_cost():
+    ingredients = pd.read_csv("ingredients.csv")
+    return ingredients.iloc[1, 1]
+
+def keci_peyniri_cost():
+    ingredients = pd.read_csv("ingredients.csv")
+    return ingredients.iloc[2, 1]
+
+def et_cost():
+    ingredients = pd.read_csv("ingredients.csv")
+    return ingredients.iloc[3, 1]
+
+def sogan_cost():
+    ingredients = pd.read_csv("ingredients.csv")
+    return ingredients.iloc[4, 1]
+
+def misir_cost():
+    ingredients = pd.read_csv("ingredients.csv")
+    return ingredients.iloc[5, 1]
+
+#beverages
+def kola_cost():
+    beverages = pd.read_csv("ingredients.csv")
+    return beverages.iloc[0, 1]
+
+def fanta_cost():
+    beverages = pd.read_csv("ingredients.csv")
+    return beverages.iloc[1, 1]
+
+def gazoz_cost():
+    beverages = pd.read_csv("ingredients.csv")
+    return beverages.iloc[2, 1]
+
+def limonata_cost():
+    beverages = pd.read_csv("ingredients.csv")
+    return beverages.iloc[3, 1]
+
+def ayran_cost():
+    beverages = pd.read_csv("ingredients.csv")
+    return beverages.iloc[4, 1]
+
