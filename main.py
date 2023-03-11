0c053c7e5af597e5c234c2160af08a9b55179d11
Barış Atasoy <109789408+the-atasoy@users.noreply.github.com>
2023-03-11 13:00:00 +0300

    translate

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 
 if __name__ == '__main__':
     from GUI_Pages.MainPage import *
-    uyg = QApplication(sys.argv)
-    pencere = MainPage()
-    pencere.show()
-    sys.exit(uyg.exec_()) 
+    app = QApplication(sys.argv)
+    window = MainPage()
+    window.show()
+    sys.exit(app.exec_())
